178de35a8d5beacf3ef7fa6892c57e2742415ca3
M3LKY <melquisedecgonzalez74@gmail.com>
2023-07-04 12:25:45 -0400

    Mm

--- src/main.js
+++ src/main.js
@@ -4,10 +4,7 @@ import App from './App.js';
 import './index.css';
 import 'tailwindcss/tailwind.css';
 
-createRoot(document.getElementById('root')).render(
-  React.createElement(
-    React.StrictMode,
-    null,
-    React.createElement(App)
-  )
-);
+const rootElement = document.getElementById('root');
+const strictModeElement = React.createElement(React.StrictMode, null, React.createElement(App));
+
+createRoot(rootElement).render(strictModeElement);
